d569294ed38aed4eff5b2a8a288e83821f8c0289
Karthick Ragavendran <karthickragavendran@Karthicks-Mac-Studio.local>
2023-05-16 20:50:20 +0530

    create web-manager app

--- apps/web-manager/tailwind.config.js
+++ apps/web-manager/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   presets: [require('../../libs/ui/tailwind.config')],
   content: ['./src/**/*.{ts,tsx}', '../../libs/ui/**/*.{ts,tsx}'],
   important: true,
-  theme: {},
+  theme: {
+    extend: {},
+  },
   plugins: [],
 }
