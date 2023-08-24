927bc2dad7c7b3c215fad78580b4e1e6d897542d
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-08-25 02:55:14 +0530

    cool sample data from chatgpt

---
+++ apps/api/prisma/seed/data/index.ts
@@ -0,0 +1,206 @@
+export const parkingCompanies = [
+  {
+    companyName: 'Chennai Chariots',
+    companyDescription: 'We park cars better than Rajinikanth fights villains!',
+    garages: [
+      {
+        displayName: 'Marina Marvel',
+        description:
+          'A parking spot so close to the beach, your car might just learn to surf!',
+        address: '42 Marina Beach Road, Chennai',
+      },
+      {
+        displayName: 'T. Nagar Treasure',
+        description:
+          'Park here and your car will shop for you at Saravana Stores!',
+        address: '15 Usman Road, T. Nagar, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Auto Anna',
+    companyDescription:
+      'Where your auto feels at home, and sometimes even breaks into a Bharatanatyam!',
+    garages: [
+      {
+        displayName: 'Mylapore Mirth',
+        description:
+          "Your car's spiritual home, right next to Kapaleeshwarar Temple!",
+        address: '3 Luz Church Road, Mylapore, Chennai',
+      },
+      {
+        displayName: 'Velachery Vroom',
+        description:
+          'Park here and your car might just catch a local train to Beach Station!',
+        address: '10 Velachery Main Road, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Parking Pongal',
+    companyDescription:
+      'We treat your car like a Pongal pot - with care, warmth, and a touch of ghee!',
+    garages: [
+      {
+        displayName: 'Adyar Amusement',
+        description: 'A parking spot so cool, your car will refuse to leave!',
+        address: '5 Adyar Bridge Road, Chennai',
+      },
+      {
+        displayName: 'Kodambakkam Kraziness',
+        description:
+          'Park here and your car might just land a role in the next Kollywood blockbuster!',
+        address: '22 Arcot Road, Kodambakkam, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Curry Cars',
+    companyDescription:
+      'We spice up your parking experience like a Chettinad curry!',
+    garages: [
+      {
+        displayName: 'Besant Beach Bunker',
+        description:
+          'A parking spot so breezy, your car will want to build sandcastles!',
+        address: '7 Besant Nagar Beach Road, Chennai',
+      },
+      {
+        displayName: 'Pallavaram Paradise',
+        description:
+          'Park here and your car might just take off at the nearby airport!',
+        address: '33 Pallavaram High Road, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Madras Motor Mansion',
+    companyDescription:
+      'We park your cars with the elegance of a Bharatanatyam mudra!',
+    garages: [
+      {
+        displayName: 'Egmore Elegance',
+        description:
+          'A parking spot so classy, your car will start speaking in poetic Tamil!',
+        address: '12 Egmore Museum Road, Chennai',
+      },
+      {
+        displayName: 'Triplicane Triumph',
+        description:
+          'Park here and your car might just join the cricket match at Chepauk Stadium!',
+        address: '9 Wallajah Road, Triplicane, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Filter Coffee Parking',
+    companyDescription:
+      'We brew the perfect parking spot for your car, just like your favorite filter coffee!',
+    garages: [
+      {
+        displayName: 'Anna Nagar Aroma',
+        description:
+          'A parking spot so central, your car will feel like the superstar of Chennai!',
+        address: '2nd Avenue, Anna Nagar, Chennai',
+      },
+      {
+        displayName: 'Porur Perfection',
+        description:
+          "Park here and your car will enjoy the IT crowd's company!",
+        address: 'Mount Poonamallee Road, Porur, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Dosai Drive-In',
+    companyDescription:
+      'We fold your parking worries like a crispy Masala Dosai!',
+    garages: [
+      {
+        displayName: 'Saidapet Satisfaction',
+        description:
+          'A parking spot so satisfying, your car will hum A.R. Rahman tunes!',
+        address: 'Anna Salai, Saidapet, Chennai',
+      },
+      {
+        displayName: 'Thiruvanmiyur Thrill',
+        description:
+          'Park here and your car might just take a dip in the Bay of Bengal!',
+        address: 'East Coast Road, Thiruvanmiyur, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Autokaaran',
+    companyDescription: 'Where your car parks with the style of a superstar!',
+    garages: [
+      {
+        displayName: 'Cinema kaaran',
+        description:
+          'A parking spot so cinematic, your car might get a role in the next blockbuster!',
+        address: 'Vadapalani, Chennai',
+      },
+      {
+        displayName: 'Marina Magic',
+        description:
+          'Park here and your car will enjoy the sea breeze and beach cricket!',
+        address: 'Marina Beach Road, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Chennai Chariots',
+    companyDescription:
+      'We park your vehicles with the warmth of a traditional Tamil welcome!',
+    garages: [
+      {
+        displayName: 'Mylapore Melody',
+        description:
+          'A parking spot so divine, your car will feel blessed at Kapaleeshwarar Temple!',
+        address: 'Mylapore, Chennai',
+      },
+      {
+        displayName: 'T. Nagar Treasure',
+        description:
+          'Park here and your car might go on a shopping spree at Saravana Stores!',
+        address: 'Usman Road, T. Nagar, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Idli Inn',
+    companyDescription:
+      'Soft, safe, and steaming hot parking spots, just like your favorite idlis!',
+    garages: [
+      {
+        displayName: 'Velachery Vehicle Villa',
+        description:
+          'A parking spot so tech-savvy, your car will start coding!',
+        address: 'Velachery Main Road, Chennai',
+      },
+      {
+        displayName: 'Adyar Auto Abode',
+        description: 'Park here and your car might enroll in IIT Madras!',
+        address: 'Adyar, Chennai',
+      },
+    ],
+  },
+  {
+    companyName: 'Lungi Lounge',
+    companyDescription:
+      'We wrap your car in comfort, just like a traditional lungi!',
+    garages: [
+      {
+        displayName: 'Koyambedu Kingdom',
+        description:
+          'A parking spot so bustling, your car will dance to local tunes!',
+        address: 'Koyambedu, Chennai',
+      },
+      {
+        displayName: 'Poes Garden Palace',
+        description: 'Park here and your car might run for political office!',
+        address: 'Poes Garden, Chennai',
+      },
+    ],
+  },
+]
